e04cb24b985704955b295f257b0c3094964145e1
John James Ospina <ospina.john@correounivalle.edu.co>
2022-10-12 12:26:20 -0500

    metodos crud de product terminado

--- backend/controllers/productsController.js
+++ backend/controllers/productsController.js
@@ -2,23 +2,119 @@ import productsModel from '../models/productsModel.js';
 
 // Obtener todos los productos
 export const getProducts = async (req, res, next) => {
-    try {
-        const products = await productsModel.find({});
-        res.status(200).json(products);
-    } catch (error) {
-        next(error);
-    }
+	try {
+		const products = await productsModel.find();
+		res.status(200).json({
+			success: true,
+			count: products.length,
+			data: products.map(product => {
+				return {
+					_id: product._id,
+					name: product.name,
+					price: product.price,
+					request: {
+						type: 'GET',
+						url: 'http://localhost:4000/api/product/' + product._id,
+					},
+				};
+			}),
+		});
+	} catch (error) {
+		res.status(500).json({
+			success: false,
+			error: 'Error en el servidor',
+		});
+	}
+};
+
+// Obtener un producto por id
+export const getProductById = async (req, res, next) => {
+	try {
+		const product = await productsModel.findById(req.params.id);
+		if (!product) {
+			return res.status(404).json({
+				success: false,
+				message: 'Producto no encontrado',
+			});
+		}
+		res.status(200).json({
+			success: true,
+			data: product,
+		});
+	} catch (error) {
+		res.status(500).json({
+			success: false,
+			error: 'Error en el servidor',
+		});
+	}
 };
 
 // Creando productos
 export const newProduct = async (req, res, next) => {
-    try {
-        const product = await productsModel.create(req.body);
-        res.status(201).json({
-            success: true,
-            product,
-        });
-    } catch (error) {
-        next(error);
-    }
+	try {
+		const product = await productsModel.create(req.body);
+		res.status(201).json({
+			success: true,
+			data: product,
+		});
+	} catch (error) {
+		res.status(500).json({
+			success: false,
+			error: 'Error en el servidor',
+		});
+	}
+};
+
+// Actualizar un producto
+export const updateProduct = async (req, res, next) => {
+	try {
+		let product = await productsModel.findById(req.params.id);
+		if (!product) {
+			// Si no existe el producto termina el proceso
+			return res.status(404).json({
+				success: false,
+				message: 'Producto no encontrado',
+			});
+		}
+		// Si el producto si existe, entonces se actualiza
+		product = await productModel.findByIdAndUpdate(req.params.id, product, {
+			new: true,
+			runValidators: true,
+		});
+		res.status(200).json({
+			success: true,
+			data: product,
+		});
+	} catch (error) {
+		res.status(500).json({
+			success: false,
+			error: 'Error en el servidor',
+		});
+	}
+};
+
+// Eliminar un producto
+export const deleteProduct = async (req, res, next) => {
+	try {
+		const product = await productsModel.findByIdAndRemove(req.params.id);
+		if (!product) {
+			return res.status(404).json({
+				success: false,
+				message: 'Producto no encontrado',
+			});
+		}
+		res.status(200).json({
+			success: true,
+			data: {
+				message: 'Producto eliminado',
+				id: product.id,
+				name: product.name,
+			},
+		});
+	} catch (error) {
+		res.status(500).json({
+			success: false,
+			error: 'Error en el servidor',
+		});
+	}
 };
--- backend/routes/products.routes.js
+++ backend/routes/products.routes.js
@@ -1,8 +1,12 @@
 import { Router } from 'express';
-import { getProducts, newProduct } from '../controllers/productsController.js';
+import { deleteProduct, getProductById, getProducts, newProduct, updateProduct } from '../controllers/productsController.js';
 
 const router = Router();
 router.get('/products', getProducts); // Obtener todos los productos
+router.get('/product/:id', getProductById); // Obtener un producto por id
 router.post('/product/new', newProduct); // Creando producto
+router.put('/product/:id', updateProduct); // Actualizando producto
+router.delete('/product/:id', deleteProduct); // Eliminar producto
+
 
 export default router;
